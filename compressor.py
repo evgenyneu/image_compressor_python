8f38fa725d44798a275b4341ff1b357db7f8f448
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2019-06-29 00:50:28 +1000

    Multiply matrices

--- compressor.py
+++ compressor.py
@@ -1,6 +1,54 @@
 import imageio
 
 
+def matrix_multiply(a, b):
+    """
+    Multiplies two matrices.
+
+    Parameters
+    ----------
+    a, b : list of numbers
+        Matrices to multiply.
+
+    Returns
+    -------
+    list of numbers
+        The product of the two matrices.
+    """
+
+    if len(a) == 0 or len(b) == 0 or len(b[0]) == 0:
+        raise ValueError(f"Supplied matrix is empty.")
+
+    if len(a[0]) != len(b):
+        raise ValueError(f"Incompatible dimensions.")
+
+    # We use the following letters for matrix dimensions:
+    #   a is m by n,
+    #   b is n by p.
+
+    m = len(a)
+    n = len(a[0])
+    p = len(b[0])
+    product = [[0] * p for i in range(m)]
+
+    for i in range(m):
+        for j in range(p):
+            element = 0
+
+            if len(a[i]) != n:
+                raise ValueError(f"Unequal number of elements in rows of a.")
+
+            for k in range(0, n):
+                if len(b[k]) != p:
+                    raise ValueError(f"Unequal number of elements in columns of b.")
+
+                element += a[i][k] * b[k][j]
+
+            product[i][j] = element
+
+    return product
+
+
 def load_image(path):
     """
     Loads image and returns the data
@@ -17,3 +65,7 @@ def load_image(path):
     """
 
     return imageio.imread(path)
+
+
+if __name__ == '__main__':
+    load_image('marmite_500x500_gray.bmp')
--- compressor_test.py
+++ compressor_test.py
@@ -1,4 +1,22 @@
-from compressor import load_image
+from compressor import load_image, matrix_multiply
+import pytest
+
+
+def test_matrix_multiply():
+    a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    b = [[1, 5], [1, 2], [1, 1]]
+
+    result = matrix_multiply(a, b)
+
+    assert result == [[6, 12], [15, 36], [24, 60]]
+
+
+def test_matrix_multiply_incompatible_dimensions():
+    a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    b = [[1, 5, 3], [1, 2, 3]]
+
+    with pytest.raises(ValueError):
+        matrix_multiply(a, b)
 
 
 def test_load_image():
