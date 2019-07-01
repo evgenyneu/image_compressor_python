06b4551add20daff4c491a9d87bccd8965e75eaa
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2019-07-01 15:04:55 +1000

    Compress image

--- compressor.py
+++ compressor.py
@@ -1,15 +1,18 @@
 import imageio
-from svd import svd
+from svd import svd, singular_value_expansion
 import numpy as np
 
 
-def compress_image(path):
+def compress_image(path, path_out, terms, iterations):
     data = load_image(path)
 
-    svd_data = svd(matrix=data, max_eigenvalues=3, iterations=10)
+    svd_data = svd(matrix=data, max_eigenvalues=terms, iterations=iterations)
+    compressed_data = singular_value_expansion(svd_data)
 
-    # print(f"len(svd_data): {len(svd_data)}")
-    return 12
+    # Save to image
+    compressed_data = np.clip(compressed_data, 0, 254)
+    compressed_data = compressed_data.astype(np.uint8)
+    imageio.imwrite(path_out, compressed_data)
 
 
 def load_image(path):
--- compressor_test.py
+++ compressor_test.py
@@ -1,9 +1,15 @@
 from compressor import load_image, compress_image
+import os
 
 
 def test_compress_image():
-    # compress_image("images/marmite_500x500_gray.bmp")
-    assert 1 == 1
+    terms = 20
+    iterations = 5
+    path_out = f"images/marmite_500x500_gray_compressed_{terms}terms_{iterations}iterations.bmp"
+
+    compress_image("images/marmite_500x500_gray.bmp", path_out, terms=terms, iterations=iterations)
+    assert os.path.exists(path_out)
+    os.remove(path_out)
 
 
 def test_load_image():
